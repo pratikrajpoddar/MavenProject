5b12b59057aee92fbe182fa7100b1a46ffd662e1
pratikrajpoddar <pratikrajpoddar@gmail.com>
2026-01-29 17:59:10 +0530

    Initial Commit

---
+++ src/test/java/org/example/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import io.restassured.RestAssured;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        RestAssured.given() .baseUri("https://www.google.com/").log().all()
+                        .when().get()
+                        .then().statusCode(200).log().all();
+
+    }
+}
